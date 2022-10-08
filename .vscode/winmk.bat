d0f76b4a7ef50e9ffa231c6e0dae33c6a0f9fb3a
chenrixin <chenrixin@zh-jielic.om>
2022-10-08 10:15:35 +0800

    删除代码文件，保留issue的附件

--- .vscode/winmk.bat
+++
@@ -1,3 +0,0 @@
-SET SCRIPT_PATH=%~dp0%
-SET PATH=%SCRIPT_PATH%\..\tools\utils;%PATH%
-make "%1" -j %NUMBER_OF_PROCESSORS%
--- include_lib/btstack/btstack_task.h
+++
@@ -1,9 +0,0 @@
-#ifndef BTSTACK_TASK_H
-#define BTSTACK_TASK_H
-
-int btstack_init();
-int btstack_exit();
-
-void ble_bqb_test_thread_init(void);
-
-#endif
